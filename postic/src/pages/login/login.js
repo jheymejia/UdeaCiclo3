49ad287b80a8b0af7487d048952daebda7d8600b
wilcorfis <wilmerandres505@gmail.com>
2021-09-27 23:17:16 -0500

    menu bar con login auth0

--- postic/src/pages/login/login.js
+++ postic/src/pages/login/login.js
@@ -1,40 +1,26 @@
-import './login.css';
+//import "./App.css";
+import LoginButton from "../../components/LoginButton";
+import LogoutButton from "../../components/LogoutButton";
+import Profile from "../../util/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import React from 'react';
 
-export default function Login(){
-    return (
-    <div className="App">
-      <div class="col-6 mx-auto ">
-        <h1>Login</h1>
-        <form class="text-start mt-5">
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
-              Password
-            </label>
-            <input type="password" class="form-control" />
-          </div>
-          <button
-            type="submit"
-            class="btn btn-primary"
-            onSubmit="handleOnSubmit"
-          >
-            {" "}
-            Submit{" "}
-          </button>
-        </form>
-      </div>
+function Login() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <h1>Is Loading</h1>
+  }
+
+  return (
+    
+
+    
+    <div className="Login">
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      <Profile />
     </div>
   );
 }
+
+export default Login;
--- postic/src/router/AppRouter.js
+++ postic/src/router/AppRouter.js
@@ -1,14 +1,32 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 import Navigation from "../components/navigation";
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
 
 import Login from '../pages/login/login';
+const domain = 'dev-pckqg2y3.us.auth0.com';
+const clientId = 'SyyA6duWXNNe3pZhkvRYJ3gp59oqPsQT';
+console.log(domain);
+console.log(clientId);
+
+
 
 export default function AppRouter() {
     return (
         <Router>
             <Navigation />
             <Switch>
-                <Route exact path="/login" component={ Login }/>
+                <Route exact path="/login" >
+                    <React.StrictMode>
+                        <Auth0Provider
+                            domain={domain}
+                            clientId={clientId}
+                            redirectUri='http://localhost:3000/login'
+                        >
+                            <Login />
+                        </Auth0Provider>
+                    </React.StrictMode>
+                </Route>
                 <Route exact path="/modulo-ventas">
                     <h2>Ventas</h2>
                 </Route>
@@ -22,6 +40,7 @@ export default function AppRouter() {
                     <h1 className="mx-auto">Error 404 Page not found</h1>
                 </Route>
             </Switch>
+
         </Router>
     )
 }
